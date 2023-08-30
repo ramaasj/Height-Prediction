871fa6dbecbdc241636afb49415932aa515fc4cb
Rama <ramajanitra999@gmail.com>
2023-08-30 18:27:17 +0700

    Initial Commit

---
+++ measureprewitt.py
@@ -0,0 +1,148 @@
+# import the necessary packages
+import argparse
+from PIL import Image
+import cv2
+import imutils
+import numpy as np
+from imutils import contours, perspective
+from scipy.spatial import distance as dist
+
+
+def midpoint(ptA, ptB):
+	return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to the input image")
+ap.add_argument("-w", "--width", type=float, required=True,
+	help="width of the left-most object in the image (in inches)")
+args = vars(ap.parse_args())
+
+
+
+# Open the image
+img = np.array(Image.open(args["image"])).astype(np.uint8)
+
+# Apply gray scale
+gray_img = np.round(0.299 * img[:, :, 0] +
+                    0.587 * img[:, :, 1] +
+                    0.114 * img[:, :, 2]).astype(np.uint8)
+
+# Prewitt Operator
+h, w = gray_img.shape
+# define filters
+horizontal = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])  # s2
+vertical = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])  # s1
+
+# define images with 0s
+newgradientImage = np.zeros((h, w))
+
+# offset by 1
+for i in range(1, h - 1):
+    for j in range(1, w - 1):
+        horizontalGrad = (horizontal[0, 0] * gray_img[i - 1, j - 1]) + \
+                         (horizontal[0, 1] * gray_img[i - 1, j]) + \
+                         (horizontal[0, 2] * gray_img[i - 1, j + 1]) + \
+                         (horizontal[1, 0] * gray_img[i, j - 1]) + \
+                         (horizontal[1, 1] * gray_img[i, j]) + \
+                         (horizontal[1, 2] * gray_img[i, j + 1]) + \
+                         (horizontal[2, 0] * gray_img[i + 1, j - 1]) + \
+                         (horizontal[2, 1] * gray_img[i + 1, j]) + \
+                         (horizontal[2, 2] * gray_img[i + 1, j + 1])
+
+        verticalGrad = (vertical[0, 0] * gray_img[i - 1, j - 1]) + \
+                       (vertical[0, 1] * gray_img[i - 1, j]) + \
+                       (vertical[0, 2] * gray_img[i - 1, j + 1]) + \
+                       (vertical[1, 0] * gray_img[i, j - 1]) + \
+                       (vertical[1, 1] * gray_img[i, j]) + \
+                       (vertical[1, 2] * gray_img[i, j + 1]) + \
+                       (vertical[2, 0] * gray_img[i + 1, j - 1]) + \
+                       (vertical[2, 1] * gray_img[i + 1, j]) + \
+                       (vertical[2, 2] * gray_img[i + 1, j + 1])
+
+        # Edge Magnitude
+        mag = np.sqrt(pow(horizontalGrad, 2.0) + pow(verticalGrad, 2.0))
+        newgradientImage[i - 1, j - 1] = mag
+
+prewittxy = newgradientImage
+prewittxy = cv2.dilate(prewittxy, None, iterations=1)
+prewittxy = cv2.erode(prewittxy, None, iterations=1)
+prewittxy = np.uint8(prewittxy)
+
+
+# find contours in the edge map
+cnts = cv2.findContours(prewittxy.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+# sort the contours from left-to-right and initialize the
+# 'pixels per metric' calibration variable
+(cnts, _) = contours.sort_contours(cnts)
+pixelsPerMetric = None
+
+
+# loop over the contours individually
+for c in cnts:
+	# if the contour is not sufficiently large, ignore it
+	if cv2.contourArea(c) < 100:
+		continue
+	# compute the rotated bounding box of the contour
+	orig = img.copy()
+	box = cv2.minAreaRect(c)
+	box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+	box = np.array(box, dtype="int")
+	# order the points in the contour such that they appear
+	# in top-left, top-right, bottom-right, and bottom-left
+	# order, then draw the outline of the rotated bounding
+	# box
+	box = perspective.order_points(box)
+	cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
+	# loop over the original points and draw them
+	for (x, y) in box:
+		cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
+
+
+	# unpack the ordered bounding box, then compute the midpoint
+	# between the top-left and top-right coordinates, followed by
+	# the midpoint between bottom-left and bottom-right coordinates
+	(tl, tr, br, bl) = box
+	(tltrX, tltrY) = midpoint(tl, tr)
+	(blbrX, blbrY) = midpoint(bl, br)
+	# compute the midpoint between the top-left and top-right points,
+	# followed by the midpoint between the top-righ and bottom-right
+	(tlblX, tlblY) = midpoint(tl, bl)
+	(trbrX, trbrY) = midpoint(tr, br)
+	# draw the midpoints on the image
+	cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
+	cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
+	cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
+	cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
+	# draw lines between the midpoints
+	cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
+		(255, 0, 255), 2)
+	cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
+		(255, 0, 255), 2)
+
+
+	# compute the Euclidean distance between the midpoints
+	dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+	dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+	# if the pixels per metric has not been initialized, then
+	# compute it as the ratio of pixels to supplied metric
+	# (in this case, inches)
+	if pixelsPerMetric is None:
+		pixelsPerMetric = dB / args["width"]
+
+
+	# compute the size of the object
+	dimA = dA / pixelsPerMetric
+	dimB = dB / pixelsPerMetric
+	# draw the object sizes on the image
+	cv2.putText(orig, "{:.1f}in".format(dimA),
+		(int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
+		0.65, (255, 255, 255), 2)
+	cv2.putText(orig, "{:.1f}in".format(dimB),
+		(int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
+		0.65, (255, 255, 255), 2)
+	# show the output image
+	cv2.imshow("Image", orig)
+	cv2.waitKey(0)
